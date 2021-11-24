4e61901f75fe4080bbe8cc001cb20fe2635b7df5
Tariq Al-Khasib <talkhasib@fb.com>
2021-11-24 11:21:31 -0800

    Add LTE Integ Test reports
    Signed-off-by: Tariq Al-Khasib <talkhasib@fb.com>

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -40,7 +40,7 @@
           <b-th colspan="1"><span class="sr-only">ID</span></b-th>
           <b-th variant="light" colspan="4">Metadata</b-th>
           <b-th variant="info" colspan="5">Builds</b-th>
-          <b-th variant="primary" colspan="3">Workers</b-th>
+          <b-th variant="primary" colspan="4">Workers</b-th>
         </b-tr>
       </template>
       <template #cell(build_id)="data">
@@ -71,6 +71,9 @@
         <b-icon v-on:click="show_html(data.value.msg)" :icon="`${data.value.icon}`" scale="2" :variant="`${data.value.variant}`"></b-icon>
       </template>
       <template #cell(w_wl5g)="data">
+        <b-icon v-on:click="show_html(data.value.msg)" :icon="`${data.value.icon}`" scale="2" :variant="`${data.value.variant}`"></b-icon>
+      </template>
+      <template #cell(w_lte_integ)="data">
         <b-icon v-on:click="show_html(data.value.msg)" :icon="`${data.value.icon}`" scale="2" :variant="`${data.value.variant}`"></b-icon>
       </template>
     </b-table>
@@ -277,6 +280,25 @@ export default {
               return newValue
             }
           },
+          {
+            key: 'w_lte_integ',
+            label: 'LTE INTEG',
+            formatter: value => {
+              let newValue = {
+                "icon": "circle",
+                "variant": "secondary",
+                "msg": value.report
+              }
+              if (value.verdict == "pass") {
+                newValue.icon = "check-circle-fill"
+                newValue.variant = "success"
+              } else if(value.verdict == "fail") {
+                newValue.icon = "exclamation-circle-fill"
+                newValue.variant = "danger"
+              }
+              return newValue
+            },
+          },
         ],
       currentPage: 1,
       rowsPerPage: 20,
@@ -320,6 +342,10 @@ export default {
         if (dbObject.workers.wl_lab_5g.reports) {
           wl5g_report = (key in dbObject.workers.wl_lab_5g.reports) ? dbObject.workers.wl_lab_5g.reports[key] : {"verdict": "not_present"};
         }
+        var lte_integ_report = {};
+        if (dbObject.workers.lte_integ_test.reports) {
+          lte_integ_report = (key in dbObject.workers.lte_integ_test.reports) ? dbObject.workers.lte_integ_test.reports[key] : {"verdict": "not_present"};
+        }
 
         this.items.push(
           {
@@ -336,6 +362,7 @@ export default {
             w_spirent: spirent_report,
             w_tvm: tvm_report,
             w_wl5g: wl5g_report,
+            w_lte_integ: lte_integ_report,
           })
       }
       //console.log(this.items)
